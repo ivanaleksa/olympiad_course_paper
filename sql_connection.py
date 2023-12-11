2bd9c208ff336c984fc2722b02b03f846b60d662
Ivan <ivan.aleksandrov03@mail.ru>
2023-12-11 15:19:44 +0400

    more than one sql object was forbidden

--- sql_connection.py
+++ sql_connection.py
@@ -4,6 +4,12 @@ from env_vars_local import db_params
 
 
 class SqlConnection:
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self):
         self.conn = psycopg2.connect(**db_params)
         self.cursor = self.conn.cursor()
