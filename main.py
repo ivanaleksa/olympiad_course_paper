83f705f42db440aecdf9a24c3077dc6a86d6ac5a
Ivan <ivan.aleksandrov03@mail.ru>
2023-12-12 20:01:22 +0400

    added a method for table rendering and rander on each page

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import tkinter as tk
+from tkinter import ttk
+from sql_connection import SqlConnection
 
 class OlympiadApp:
     def __init__(self, master):
+        self.conn = SqlConnection()
         self.master = master
+        self.current_table = None
 
         # adjust the main window
         master.title("Olympiad App")
@@ -15,55 +19,245 @@ class OlympiadApp:
         self.side_menu.pack(side=tk.LEFT, fill=tk.Y)
 
         # Create buttons for the side menu
-        self.button1 = tk.Button(self.side_menu, text="Участники", command=self.option1)
-        self.button1.pack(pady=30)
+        self.participant_button = tk.Button(self.side_menu, text="Participants", command=self.participant_option)
+        self.participant_button.pack(pady=30, fill=tk.X)
 
-        self.button2 = tk.Button(self.side_menu, text="Результаты", command=self.option2)
-        self.button2.pack(pady=30, anchor='center')
+        self.result_button = tk.Button(self.side_menu, text="Results", command=self.result_option)
+        self.result_button.pack(pady=30, fill=tk.X)
 
-        self.button3 = tk.Button(self.side_menu, text="Расписание\nстартов", command=self.option3)
-        self.button3.pack(pady=30, anchor='center')
+        self.schedule_button = tk.Button(self.side_menu, text="Schedule", command=self.schedule_option)
+        self.schedule_button.pack(pady=30, fill=tk.X)
 
-        self.button4 = tk.Button(self.side_menu, text="Виды спорта", command=self.option4)
-        self.button4.pack(pady=30, anchor='center')
+        self.sport_types_button = tk.Button(self.side_menu, text="Sport types", command=self.sport_types_option)
+        self.sport_types_button.pack(pady=30, fill=tk.X)
 
-        self.button5 = tk.Button(self.side_menu, text="Спортивные\nплощадки", command=self.option5)
-        self.button5.pack(pady=30, anchor='center')
+        self.sportground_button = tk.Button(self.side_menu, text="Sportgrounds", command=self.sportground_option)
+        self.sportground_button.pack(pady=30, fill=tk.X)
 
-        self.button6 = tk.Button(self.side_menu, text="Страны", command=self.option6)
-        self.button6.pack(pady=30, anchor='center')
+        self.country_button = tk.Button(self.side_menu, text="Countries", command=self.country_option)
+        self.country_button.pack(pady=30, fill=tk.X)
 
-        self.button7 = tk.Button(self.side_menu, text="Отчеты", command=self.option7)
-        self.button7.pack(pady=30, anchor='center')
+        self.report_button = tk.Button(self.side_menu, text="Reports", command=self.report_option)
+        self.report_button.pack(pady=30, fill=tk.X)
 
         # Create a main content area
         self.main_content = tk.Frame(master, bg="white", padx=20, pady=20)
         self.main_content.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
         # Add a label to the main content area
-        self.label = tk.Label(self.main_content, text="Здравствуйте! Выберете в боковом меню нужный раздел", font=("Arial", 24))
+        self.label = tk.Label(self.main_content, text="Welcome! Choose what you need on the side bar!", font=("Arial", 24))
         self.label.pack(pady=10)
 
-    def option1(self):
-        self.label.config(text="You selected Option 1")
+        # Add a table
+        self.table_frame = tk.Frame(self.main_content, bg="white")
+        self.table_frame.pack(pady=20, fill=tk.BOTH, expand=True)
 
-    def option2(self):
-        self.label.config(text="You selected Option 2")
+    def participant_option(self):
+        self.label.config(text="Participants page")
+        self.create_and_fill_table(('ID',
+                                    'Country code',
+                                    'Name',
+                                    'Surname', 
+                                    'Birthdate'), 
+                                    'SELECT * FROM olympiad.participants')
 
-    def option3(self):
-        self.label.config(text="You selected Option 3")
+    def result_option(self):
+        self.label.config(text="Results page")
+        self.create_and_fill_table(('ID',
+                                    'Sport type',
+                                    'Sportground',
+                                    'Result',
+                                    'Place'),
+                                    'SELECT * FROM olympiad.start_results')
 
-    def option4(self):
-        self.label.config(text="You selected Option 4")
+    def schedule_option(self):
+        self.label.config(text="Schedule page")
+        self.create_and_fill_table(('ID',
+                                    'Sport type',
+                                    'Date',
+                                    'Time',
+                                    'Sport ground'),
+                                    'SELECT * FROM olympiad.starts_schedule')
 
-    def option5(self):
-        self.label.config(text="You selected Option 5")
+    def sport_types_option(self):
+        self.label.config(text="Sport types")
+        self.create_and_fill_table(('ID',
+                                    'Title',
+                                    'Type',
+                                    'Desciption',
+                                    'Season'),
+                                    'SELECT * FROM olympiad.sports_type')
 
-    def option6(self):
-        self.label.config(text="You selected Option 6")
+    def sportground_option(self):
+        self.label.config(text="Sportgrounds page")
+        self.create_and_fill_table(('ID',
+                                    'Title',
+                                    'Placement',
+                                    'Allowed sports'),
+                                    'SELECT * FROM olympiad.sports_grounds')
 
-    def option7(self):
+    def country_option(self):
+        self.label.config(text="Countries page")
+        self.create_and_fill_table(('ID',
+                                    'Title'),
+                                    'SELECT * FROM olympiad.countries')
+
+    def report_option(self):
         self.label.config(text="You selected Option 7")
+
+    def create_and_fill_table(self, columns: tuple, query: str) -> None:
+        if self.current_table:
+            self.current_table.destroy()
+
+        # Add a table
+        self.tree = ttk.Treeview(self.table_frame, columns=columns, show="headings")
+        self.tree.column("#0", width=0, stretch=False)
+        for col in self.tree["columns"]:
+            self.tree.heading(col, text=col)
+            self.tree.column(col)        
+
+        # Fill the table
+        for row in self.conn.execute_query(query):
+            self.tree.insert("", tk.END, values=row)
+
+        self.tree.pack(fill=tk.BOTH, expand=True)
+        self.current_table = self.tree
+
+
+
+def main():
+    root = tk.Tk()
+    app = OlympiadApp(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
+import tkinter as tk
+from tkinter import ttk
+from sql_connection import SqlConnection
+
+class OlympiadApp:
+    def __init__(self, master):
+        self.conn = SqlConnection()
+        self.master = master
+        self.current_table = None
+
+        # adjust the main window
+        master.title("Olympiad App")
+        master.geometry("1280x720")
+        master.minsize(1280, 720)
+        master.maxsize(1280, 720)
+
+        # Create a side menu
+        self.side_menu = tk.Frame(master, width=150, bg="lightgray")
+        self.side_menu.pack(side=tk.LEFT, fill=tk.Y)
+
+        # Create buttons for the side menu
+        self.participant_button = tk.Button(self.side_menu, text="Participants", command=self.participant_option)
+        self.participant_button.pack(pady=30, fill=tk.X)
+
+        self.result_button = tk.Button(self.side_menu, text="Results", command=self.result_option)
+        self.result_button.pack(pady=30, fill=tk.X)
+
+        self.schedule_button = tk.Button(self.side_menu, text="Schedule", command=self.schedule_option)
+        self.schedule_button.pack(pady=30, fill=tk.X)
+
+        self.sport_types_button = tk.Button(self.side_menu, text="Sport types", command=self.sport_types_option)
+        self.sport_types_button.pack(pady=30, fill=tk.X)
+
+        self.sportground_button = tk.Button(self.side_menu, text="Sportgrounds", command=self.sportground_option)
+        self.sportground_button.pack(pady=30, fill=tk.X)
+
+        self.country_button = tk.Button(self.side_menu, text="Countries", command=self.country_option)
+        self.country_button.pack(pady=30, fill=tk.X)
+
+        self.report_button = tk.Button(self.side_menu, text="Reports", command=self.report_option)
+        self.report_button.pack(pady=30, fill=tk.X)
+
+        # Create a main content area
+        self.main_content = tk.Frame(master, bg="white", padx=20, pady=20)
+        self.main_content.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+        # Add a label to the main content area
+        self.label = tk.Label(self.main_content, text="Welcome! Choose what you need on the side bar!", font=("Arial", 24))
+        self.label.pack(pady=10)
+
+        # Add a table
+        self.table_frame = tk.Frame(self.main_content, bg="white")
+        self.table_frame.pack(pady=20, fill=tk.BOTH, expand=True)
+
+    def participant_option(self):
+        self.label.config(text="Participants page")
+        self.create_and_fill_table(('ID',
+                                    'Country code',
+                                    'Name',
+                                    'Surname', 
+                                    'Birthdate'), 
+                                    'SELECT * FROM olympiad.participants')
+
+    def result_option(self):
+        self.label.config(text="Results page")
+        self.create_and_fill_table(('ID',
+                                    'Sport type',
+                                    'Sportground',
+                                    'Result',
+                                    'Place'),
+                                    'SELECT * FROM olympiad.start_results')
+
+    def schedule_option(self):
+        self.label.config(text="Schedule page")
+        self.create_and_fill_table(('ID',
+                                    'Sport type',
+                                    'Date',
+                                    'Time',
+                                    'Sport ground'),
+                                    'SELECT * FROM olympiad.starts_schedule')
+
+    def sport_types_option(self):
+        self.label.config(text="Sport types")
+        self.create_and_fill_table(('ID',
+                                    'Title',
+                                    'Type',
+                                    'Desciption',
+                                    'Season'),
+                                    'SELECT * FROM olympiad.sports_type')
+
+    def sportground_option(self):
+        self.label.config(text="Sportgrounds page")
+        self.create_and_fill_table(('ID',
+                                    'Title',
+                                    'Placement',
+                                    'Allowed sports'),
+                                    'SELECT * FROM olympiad.sports_grounds')
+
+    def country_option(self):
+        self.label.config(text="Countries page")
+        self.create_and_fill_table(('ID',
+                                    'Title'),
+                                    'SELECT * FROM olympiad.countries')
+
+    def report_option(self):
+        self.label.config(text="You selected Option 7")
+
+    def create_and_fill_table(self, columns: tuple, query: str) -> None:
+        if self.current_table:
+            self.current_table.destroy()
+
+        # Add a table
+        self.tree = ttk.Treeview(self.table_frame, columns=columns, show="headings")
+        self.tree.column("#0", width=0, stretch=False)
+        for col in self.tree["columns"]:
+            self.tree.heading(col, text=col)
+            self.tree.column(col)        
+
+        # Fill the table
+        for row in self.conn.execute_query(query):
+            self.tree.insert("", tk.END, values=row)
+
+        self.tree.pack(fill=tk.BOTH, expand=True)
+        self.current_table = self.tree
+
 
 
 def main():
